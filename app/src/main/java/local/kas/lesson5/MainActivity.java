c9cef26e44a686c4971abd4836c3c2157e90054f
polotenchik <yashikpolotenchika@gmail.com>
2021-06-25 15:36:16 +0300

    first commit

---
+++ app/src/main/java/local/kas/lesson5/MainActivity.java
@@ -0,0 +1,63 @@
+package local.kas.lesson5;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button btnOpen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        btnOpen = findViewById(R.id.btnOpen);
+
+        btnOpen.setOnClickListener(v -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, 100);
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.CAMERA
+            }, 100);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+//        imageView.setImageBitmap((Bitmap) data.getExtras().get("data"));  // works but poor image quality
+
+//        Parcelable[] parcelables = data.getParcelableArrayExtra(data.EXTRA_STREAM);  // doesn't work
+
+//        Parcelable parcelable = data.getParcelableExtra(Intent.EXTRA_STREAM);  // doesn't work
+
+//        imageView.setImageURI(data.getParcelableExtra(Intent.EXTRA_STREAM));  // doesn't work
+        Uri a = data.getData();
+        imageView.setImageURI(data.getData());  // doesn't work
+    }
+}
